bdc84dfb48aa7bab5310f8375ce92473c065f05d
Subha <msubhadeep9635@gmail.com>
2024-06-24 18:59:52 +0530

    one comment added

--- q1/HelloWorld.java
+++ q1/HelloWorld.java
@@ -1,7 +1,11 @@
 // Write a program to display "Hello World!".
 
 // NOTE: There is no need to write package here.
-// It is done because the code of every question is inside a separate folder.
+// It is done to avoid errors because the code of every question is inside a separate folder.
+// If you want to do same, then while compiling you must, for example-
+// javac q1/HelloWorld.java
+// java q1.HelloWorld
+
 package q1;
 class HelloWorld{
     public static void main(String args[]){
